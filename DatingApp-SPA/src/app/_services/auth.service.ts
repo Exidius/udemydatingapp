1c8430a5e968eb20e636bc4a77882779fef6a157
Barad Balázs <barad95@gmail.com>
2019-02-01 15:27:30 +0100

    section 4

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -16,8 +16,12 @@ login(model: any) {
        const user = response;
        if (user) {
          localStorage.setItem('token', user.token);
-       }
+        }
      })
    );
-}
+  }
+
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'register', model);
+  }
 }
